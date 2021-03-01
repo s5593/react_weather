003d1b7d97d83659839d904d072ce39be818775d
s5593 <rlaqjatn5593@gmail.com>
2021-03-01 22:10:36 +0900

    1

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import MainScreen from './Components/MainScreen';
+import { Icon } from 'native-base';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class MainScreen extends Component {
+
+  // navigationOptions 코드 추가
+  static navigationOptions = {
+    headerLeft: <Icon name='ios-camera' style={{ paddingLeft:10 }}/>,
+    title: 'Instagram',
+    headerRight: <Icon name='ios-send' style={{ paddingRight:10 }}/>,
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>MainScreen</Text>
+      </View>
+    );
+  }
 }
 
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+const AppStackNavigator = createStackNavigator({
+  Main:{
+    screen: MainScreen // MainScreen 컴포넌트를 네비게이터에 등록
+  }
+});
+
+export default createAppContainer(AppStackNavigator);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
